ef9d39a23f2bb1be3b8dff5d57d3b3b7593d1871
alexstar0106 <mazaycool@gmail.com>
2023-05-03 15:56:15 +0300

    Homework

---
+++ Homework_lesson_84/script.js
@@ -0,0 +1,15 @@
+let name = 'Alexei'
+
+let userName = name
+
+console.log(userName)
+
+let headingColor
+
+let dateNowYear
+
+let firstName = 'Ivan'
+    lastName = 'Ivanov'
+    age = 20
+
+console.log(userName)
