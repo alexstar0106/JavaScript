98cb509d36052c1202869924fcba2de8e48efce4
alexstar0106 <mazaycool@gmail.com>
2023-05-24 19:07:11 +0300

    Homework

---
+++ Homework_lesson_85/script.js
@@ -0,0 +1,18 @@
+/*let fruit = "peach"
+fruit = "apple"
+
+console.log(fruit)
+
+{let car = "citroen"}
+
+console.log(car)//ошибка*/
+
+
+var fruit = "peach"
+fruit = "apple"
+
+console.log(fruit)
+
+{ var car = "citroen" }
+
+console.log(car)//citroen
