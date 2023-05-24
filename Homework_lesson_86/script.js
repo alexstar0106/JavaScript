80499ab8453d0eb7cb73cd72db8151b06c39642f
alexstar0106 <mazaycool@gmail.com>
2023-05-24 21:38:51 +0300

    Homework

---
+++ Homework_lesson_86/script.js
@@ -0,0 +1,10 @@
+const userbirthday = "01.06.1988"
+
+const color = "black"
+
+console.log(color)
+
+if(true){
+    const color = "red"
+    console.log(color)
+}// не возникает ошибка, т.к. const color находятся в разных блоках и доступны только в рамках того блока в котором находятся
