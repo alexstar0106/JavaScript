989e097a9b3683acd82c125a34d4aff1c165aff7
alexstar0106 <mazaycool@gmail.com>
2023-06-03 15:24:44 +0300

    Homework

---
+++ Homework_lesson_87/script.js
@@ -0,0 +1,23 @@
+let name = "Alex"
+
+console.log(typeof name)
+
+let age = 35
+
+console.log(typeof age)
+
+let isLearningJS = true
+
+console.log(typeof isLearningJS)
+
+let info = {
+    name: "Alex",
+    age: 35,
+    isLearningJS: true
+}
+
+console.log(typeof info)
+
+console.log(5/0)
+console.log(-5/0)
+console.log("apple"*5)
