e64bbba5a6b1cccb15ce68f917b9152bd2f38477
alexstar0106 <mazaycool@gmail.com>
2023-06-05 18:49:41 +0300

    Homework

---
+++ Homework_lesson_88/script.js
@@ -0,0 +1,11 @@
+const name = prompt("Укажите ваше имя");
+
+const age = prompt("Укажите ваш возраст", 20);
+
+const isAgree = confirm("Изучаете ли вы JavaScrip");
+
+alert("Добро пожаловать " + (name))
+
+alert("Ваш возраст " + (age) + " лет")
+
+alert("Вы изучаете JavaScrip: " + (isAgree))
