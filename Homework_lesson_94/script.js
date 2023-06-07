30e8efdeb0d15140a53b8d93fb3e0946a4377888
alexstar0106 <mazaycool@gmail.com>
2023-06-07 19:05:47 +0300

    Homework

---
+++ Homework_lesson_94/script.js
@@ -0,0 +1,11 @@
+let a = +prompt("Укажите число")
+let b = +prompt("Укажите число")
+let c = a ** b;
+
+alert("Результат: " + c)
+
+let x = +prompt("Укажите число")
+let y = +prompt("Укажите число")
+let z = x % y;
+
+console.log(z)
