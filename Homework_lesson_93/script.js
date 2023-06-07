41607936b133df09737691a70c9126612a30efaf
alexstar0106 <mazaycool@gmail.com>
2023-06-07 18:36:51 +0300

    Homework

---
+++ Homework_lesson_93/script.js
@@ -0,0 +1,27 @@
+let a = +prompt("Укажите число")
+let b = +prompt("Укажите число")
+
+let x = a + b
+
+console.log(x)
+
+let y = a * b
+
+console.log(y)
+
+let z = a - b
+
+console.log(z)
+
+let q = a / b
+
+console.log(q)
+
+// Задача
+
+const price = 26.5
+const taxRate = 0.082
+
+let taxPrice = price * taxRate + price
+
+console.log(taxPrice)
