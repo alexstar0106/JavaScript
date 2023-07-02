42b3dd2225e2b092fa6cf5947812a08e9a46e362
alexstar0106 <mazaycool@gmail.com>
2023-07-02 14:52:56 +0300

    Homework

---
+++ Homework_lesson_107/script.js
@@ -0,0 +1,25 @@
+function foo(x) {
+if (x) {
+    console.log(1)
+} else {
+    console.log(0)
+}
+}
+
+foo()
+
+
+function sum(a,b,c) {
+    sum = a + b + c;
+    console.log(sum)
+}
+
+sum(1,2,3)
+
+
+
+function text(str1,str2) {
+    console.log(str1 + str2)
+}
+
+text('over', 'one')
