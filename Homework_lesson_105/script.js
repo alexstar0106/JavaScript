9caeeaee870347dda1813439fdddf77cbd4210cb
alexstar0106 <mazaycool@gmail.com>
2023-07-02 11:24:51 +0300

    Homework

---
+++ Homework_lesson_105/script.js
@@ -0,0 +1,12 @@
+function getAge(birthYear,currentYear) {
+    console.log(currentYear - birthYear)
+}
+
+getAge(2000,2023)
+
+
+const age = function (birthYear, currentYear) {
+    console.log(currentYear - birthYear)
+}
+
+age(2000,2023)
