0d18a9b6c3fa07f6ada0804a60ad2b47672dacf7
alexstar0106 <mazaycool@gmail.com>
2023-07-10 14:43:09 +0300

    Homework

---
+++ Homework_lesson_119/script.js
@@ -0,0 +1,10 @@
+const country = ['Belgium', 'Cuba', 'Latvia', 'Peru', 'Portugal', 'Sweden', 'Austria']
+country.sort();
+country.reverse();
+console.log(country);
+
+const array = new Array(6);
+array.fill(124, 0, 2);
+array.fill(125, 2, 4);
+array.fill(126, 4, 6);
+console.log(array);
