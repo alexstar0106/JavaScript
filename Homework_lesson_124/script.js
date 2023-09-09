e1f67a16ae6b7cf40899d59b069cf719bd6531e6
alexstar0106 <mazaycool@gmail.com>
2023-09-09 15:19:18 +0300

    Homework

---
+++ Homework_lesson_124/script.js
@@ -0,0 +1,19 @@
+let x = 0;
+while(x <= 100){
+    if (x % 2 === 0) {
+        console.log(x)
+    };
+    x++
+}
+
+let n = 30;
+while(n >= 0){
+    console.log(n);
+    n--
+}
+
+let i = 10;
+while (i >= 1) {
+    console.log(i)
+    i--
+}
