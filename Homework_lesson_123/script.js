1598af0f97d34594074c8090a6a86a51984af5a5
alexstar0106 <mazaycool@gmail.com>
2023-09-09 14:27:51 +0300

    Homework

---
+++ Homework_lesson_123/script.js
@@ -0,0 +1,10 @@
+for(let i = 0; i <= 100; i += 5){
+    console.log(i);
+}
+
+let x = prompt('Введите первое число');
+let y = prompt('Введите второе число');
+
+for(; x <= y; x++){
+    console.log(x);
+}
