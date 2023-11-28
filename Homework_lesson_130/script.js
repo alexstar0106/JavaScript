b0176697b4c19b8e3a60dfe993d057aa02018678
alexstar0106 <mazaycool@gmail.com>
2023-11-28 18:19:38 +0300

    Homework

---
+++ Homework_lesson_130/script.js
@@ -0,0 +1,17 @@
+const str = 'LorEM ipSum DoLor SIt Amet, coNsecTEtur adipiscing ELIT.';
+console.log(str.toLowerCase());
+
+const str2 = 'LorEM ipSum DoLor SIt Amet, coNsecTEtur adipiscing ELIT.';
+let arg = str2.includes('w');
+    if (arg){
+        console.log('w');
+    }
+    else{
+        console.log('такого символа нет');
+    }
+
+const str3 = 'LorEM ipSum DoLor SIt Amet, coNsecTEtur adipiscing ELIT.';
+console.log(str3.lastIndexOf('i'));
+
+const str4 = 'LorEM ipSum DoLor SIt Amet, coNsecTEtur adipiscing ELIT.';
+console.log(str4.charAt(15));
