3ce50497162711bd89d87366e0acec1f099c4b3a
alexstar0106 <mazaycool@gmail.com>
2023-11-30 22:09:12 +0300

    Homework

---
+++ Homework_lesson_133/script.js
@@ -0,0 +1,19 @@
+let result = Math.floor(Math.random()*(55 - 15 + 1)+ 15);
+console.log(result);
+
+let result2 = Math.pow(result, 2);
+console.log(result2);
+
+let result3 = Math.floor(Math.random() * (30 - 1 + 1) + 1);
+console.log(result3);
+let result4 = Math.floor(Math.random() * (30 - 1 + 1) + 1);
+console.log(result4);
+let result5 = Math.floor(Math.random() * (30 - 1 + 1) + 1);
+console.log(result5);
+let result6 = Math.floor(Math.random() * (30 - 1 + 1) + 1);
+console.log(result6);
+let result7 = Math.floor(Math.random() * (30 - 1 + 1) + 1);
+console.log(result7);
+
+console.log(Math.max(result3, result4, result5, result6, result7));
+console.log(Math.min(result3, result4, result5, result6, result7));
